7b94fc3445ec8e45c1aedf1ba7f82a442468244d
dmac <samuraidan@gmail.com>
2023-12-21 15:33:48 -0800

    First pass at merke proofs

---
+++ src/validator/merkle.rs
@@ -0,0 +1,165 @@
+use borsh::BorshDeserialize;
+use borsh_derive::BorshDeserialize;
+use color_eyre::eyre::eyre;
+use eyre::Error;
+use openssl::sha;
+
+
+/// Single struct used for original data chunks (Leaves) and branch nodes (hashes of pairs of child nodes).
+#[derive(Debug, PartialEq, Clone)]
+pub struct Node {
+    pub id: [u8; HASH_SIZE],
+    pub data_hash: Option<[u8; HASH_SIZE]>,
+    pub min_byte_range: usize,
+    pub max_byte_range: usize,
+    pub left_child: Option<Box<Node>>,
+    pub right_child: Option<Box<Node>>,
+}
+
+/// Concatenated ids and offsets for full set of nodes for an original data chunk, starting with the root.
+#[derive(Debug, PartialEq, Clone)]
+pub struct Proof {
+    pub offset: usize,
+    pub proof: Vec<u8>,
+}
+
+/// Populated with data from deserialized [`Proof`] for original data chunk (Leaf [`Node`]).
+#[repr(C)]
+#[derive(BorshDeserialize, Debug, PartialEq, Clone)]
+pub struct LeafProof {
+    data_hash: [u8; HASH_SIZE],
+    notepad: [u8; NOTE_SIZE - 8],
+    offset: [u8; 8],
+}
+
+/// Populated with data from deserialized [`Proof`] for branch [`Node`] (hash of pair of child nodes).
+#[derive(BorshDeserialize, Debug, PartialEq, Clone)]
+pub struct BranchProof {
+    left_id: [u8; HASH_SIZE],
+    right_id: [u8; HASH_SIZE],
+    notepad: [u8; NOTE_SIZE - 8],
+    offset: [u8; 8],
+}
+
+/// Includes methods to deserialize [`Proof`]s.
+pub trait ProofDeserialize<T> {
+    fn try_from_proof_slice(slice: &[u8]) -> Result<T, Error>;
+    fn offset(&self) -> usize;
+}
+
+impl ProofDeserialize<LeafProof> for LeafProof {
+    fn try_from_proof_slice(slice: &[u8]) -> Result<Self, Error> {
+        let proof = LeafProof::try_from_slice(slice)?;
+        Ok(proof)
+    }
+    fn offset(&self) -> usize {
+        usize::from_be_bytes(self.offset)
+    }
+}
+
+impl ProofDeserialize<BranchProof> for BranchProof {
+    fn try_from_proof_slice(slice: &[u8]) -> Result<Self, Error> {
+        let proof = BranchProof::try_from_slice(slice)?;
+        Ok(proof)
+    }
+    fn offset(&self) -> usize {
+        usize::from_be_bytes(self.offset)
+    }
+}
+
+pub const MAX_CHUNK_SIZE: usize = 256 * 1024;
+pub const MIN_CHUNK_SIZE: usize = 32 * 1024;
+pub const HASH_SIZE: usize = 32;
+const NOTE_SIZE: usize = 32;
+
+/// Includes a function to convert a number to a Vec of 32 bytes per the Arweave spec.
+pub trait Helpers<T> {
+    fn to_note_vec(&self) -> Vec<u8>;
+}
+
+impl Helpers<usize> for usize {
+    fn to_note_vec(&self) -> Vec<u8> {
+        let mut note = vec![0; NOTE_SIZE - 8];
+        note.extend((*self as u64).to_be_bytes());
+        note
+    }
+}
+
+/// Validates chunk of data against provided [`Proof`].
+pub fn validate_chunk(
+    mut root_id: [u8; HASH_SIZE],
+    chunk: Node,
+    proof: Proof,
+) -> Result<(), Error> {
+    match chunk {
+        Node {
+            data_hash: Some(data_hash),
+            max_byte_range,
+            ..
+        } => {
+            // Split proof into branches and leaf. Leaf is at the end and branches are ordered
+            // from root to leaf.
+            let (branches, leaf) = proof
+                .proof
+                .split_at(proof.proof.len() - HASH_SIZE - NOTE_SIZE);
+
+            // Deserialize proof.
+            let branch_proofs: Vec<BranchProof> = branches
+                .chunks(HASH_SIZE * 2 + NOTE_SIZE)
+                .map(|b| BranchProof::try_from_proof_slice(b).unwrap())
+                .collect();
+            let leaf_proof = LeafProof::try_from_proof_slice(leaf)?;
+
+            // Validate branches.
+            for branch_proof in branch_proofs.iter() {
+                // Calculate the id from the proof.
+                let id = hash_all_sha256(vec![
+                    &branch_proof.left_id,
+                    &branch_proof.right_id,
+                    &branch_proof.offset().to_note_vec(),
+                ])?;
+
+                // Ensure calculated id correct.
+                if !(id == root_id) {
+                    return Err(eyre!("Invalid Proof"));
+                }
+
+                // If the offset from the proof is greater than the offset in the data chunk,
+                // then the next id to validate against is from the left.
+                root_id = match max_byte_range > branch_proof.offset() {
+                    true => branch_proof.right_id,
+                    false => branch_proof.left_id,
+                }
+            }
+
+            // Validate leaf: both id and data_hash are correct.
+            let id = hash_all_sha256(vec![&data_hash, &max_byte_range.to_note_vec()])?;
+            if !(id == root_id) & !(data_hash == leaf_proof.data_hash) {
+                return Err(eyre!("Invalid Proof"));
+            }
+        }
+        _ => {
+            unreachable!()
+        }
+    }
+    Ok(())
+}
+
+pub fn hash_sha256(message: &[u8]) -> Result<[u8; 32], Error> {
+	let mut hasher = sha::Sha256::new();
+	hasher.update(message);
+	let result = hasher.finish();
+    Ok(result)
+}
+
+/// Returns a SHA256 hash of the the concatenated SHA256 hashes of a vector of messages.
+pub fn hash_all_sha256(messages: Vec<&[u8]>) -> Result<[u8; 32], Error> {
+    let hash: Vec<u8> = messages
+        .into_iter()
+        .map(|m| hash_sha256(m).unwrap())
+        .into_iter()
+        .flatten()
+        .collect();
+    let hash = hash_sha256(&hash)?;
+    Ok(hash)
+}
