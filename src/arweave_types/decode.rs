3c3e2ed1ee37e1a4c9bfe8dba2127e6b264f933c
dmac <samuraidan@gmail.com>
2024-01-12 11:27:48 -0800

    Clippy feedback

--- src/arweave_types/decode.rs
+++ src/arweave_types/decode.rs
@@ -10,11 +10,7 @@ impl DecodeHash for H256 {
     fn from(base64_url_string: &str) -> Result<Self, String> {
         base64_url::decode(base64_url_string)
             .map_err(|e| e.to_string())
-            .and_then(|bytes| {
-                H256::from_slice(bytes.as_slice())
-                    .try_into()
-                    .map_err(|_| format!("Length mismatch 32 - {base64_url_string}"))
-            })
+            .map(|bytes| H256::from_slice(bytes.as_slice()))
     }
 
     fn empty() -> Self {
